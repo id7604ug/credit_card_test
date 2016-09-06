d1b8b6ac921170963fb5191c175c59f111429154
id7604ug <something>
2016-09-06 13:01:45 -0500

    finish start digit test and return boolean to method

--- src/main/java/com/clara/CreditCard.java
+++ src/main/java/com/clara/CreditCard.java
@@ -25,15 +25,58 @@ public class CreditCard {
         stringScanner.close();
     }
 
-    public static boolean isValidCreditCard(String cc) {
+//    public static boolean isValidCreditCard(String cc) {
+//
+//        //TODO Replace with your code to process the credit card number, and determine if it is valid.
+//        //TODO Make sure all the tests pass!
+//        boolean testBool = false;
+//
+//        return testBool;
+//
+//    }
+    public static boolean isValidCreditCard(String cardNumber){
+        // Booleans to check validity
+        boolean checkDigit = false;
+        boolean checkLength = false;
+        boolean checkStartDigit = false;
+        // Convert the card number to an array of characters
+        char[] cardNumArray = cardNumber.toCharArray();
+        // Create boolean to check if the current number should be doubled
+        boolean doubleThis = true;
+        //todo  Start digit check
+        if (cardNumArray)
+        // Length check
+        if (cardNumArray.length == 16){
+            checkLength = true;
+        }
+        // Number addition
+        int cardNumberAdd = 0;
+        for (char number : cardNumArray) {
+            if (doubleThis){
+                if (number * 2 > 9) {
+                    cardNumberAdd += 1;
+                    cardNumberAdd += (number * 2) - 10;
+                    doubleThis = false;
+                } else {
+                    cardNumberAdd += number * 2;
+                    doubleThis = true;
+                }
+            } else {
+                cardNumberAdd += number;
+            }
+        }
+        if (cardNumberAdd % 10 == 0){
+            checkDigit = true;
+        }
 
-        //TODO Replace with your code to process the credit card number, and determine if it is valid.
-        //TODO Make sure all the tests pass!
-
-        return false;
+//        // Check if number is valid
+//        if (checkDigit && checkLength && checkStartDigit){
+//            return true;
+//        } else {
+//            return false;
+//        }
 
     }
-
 
 
 
